a6fb5d30ac68302d1acc3fc6ce0f11895cff9a8b
phanit1 <babithota5@msitprogram.net>
2020-09-30 00:07:40 +0530

    Week3Done

--- Intro to ML/Week-3/ex3/lrCostFunction.m
+++ Intro to ML/Week-3/ex3/lrCostFunction.m
@@ -9,8 +9,8 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -36,14 +36,16 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h_theta = sigmoid(X * theta);
 
+% Cost
 
+J = (1/m) * (((-1 * y') * log(h_theta)) - ((1-y') * log(1-h_theta))) + (lambda/(2*m)) * (sum(theta(2:end) .^ 2));
 
+temp = theta;
+temp(1) = 0;
 
-
-
-
-
+grad = ((1 / m) * X' * (h_theta - y)) + (lambda / m) * temp;
 
 % =============================================================
 
