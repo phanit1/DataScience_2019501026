8dbb18031b3d005cc35112a103dc2d052107766a
phanit1 <babithota5@msitprogram.net>
2020-11-10 20:49:01 +0530

    Asst4Done

---
+++ Data Mining/Assignment4/Asst4_Qstn4.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\personal\\Desktop\\MSIT\\IT\\DataScience_2019501026\\Data Mining\\Assignment4")
+
+train = read.csv("sonar_train.csv",header = FALSE)
+test = read.csv("sonar_test.csv",header = FALSE)
+summary(train)
+dim(train)
+summary(test)
+dim(test)
+
+library(rpart)
+library(rpart.plot)
+x <- train[,1:60]
+y <- as.factor(train[,61])
+model <- rpart(y~.,x,control=rpart.control(minsplit=0,minbucket=0,cp=-1, maxcompete=0, maxsurrogate=0, usesurrogate=0, xval=0,maxdepth=5))
+rpart.plot(model, box.palette="RdBu", shadow.col="gray", nn=TRUE)
+
+x_test <- test[,1:60]
+y_test <- as.factor(test[,61])
+1 - sum(y_test == predict(model,x_test,type = "class")) / length(y_test)
